e3f0041c72ca6e4e5b0b47e614909ebd152ca251
keigoNono <keigo.nono@akkodis.co.jp>
2025-07-11 06:36:54 +0000

    出力結果の記載

--- trial.py
+++ trial.py
@@ -1,4 +1,14 @@
-import pandas as pd
+import numpy as np
+x = np.array(
+    [range(1,4),
+     range(0,3)]
+)
 
-df = pd.read_excel("user_data.xlsx", index_col = "ユーザID", engine = "openpyxl") 
-print(df)
+y = np.array(
+    [[10, 11],
+     [12, 13],
+     [14, 15]]
+).reshape(2,3)
+
+print(np.concatenate([x, y]))
+print(np.concatenate([x, y], 1))
